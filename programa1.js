17c68ac4f28a497a96a423c03b707e598bbca0ef
Angel Ocariz Martinez <angelocarizmartinez@MacBook-Pro-de-Angel.local>
2021-03-21 23:16:09 -0600

    Primer código de teoría

---
+++ programa1.js
@@ -0,0 +1,66 @@
+//Código de la Sesión 1 del Módulo 2
+//Introducción a JavaScript y Node.js
+// descargar node para que me corra alv
+
+//Declaración de una Variable
+var numero = 365;
+console.log( numero );
+//Cambio de valor de la variable "numero"
+numero = 5000;
+//Impresión del valor de la variable "Numero" 
+console.log( numero );
+
+//Declaración de una Constante
+const mensaje = "Bienvenido a Ciudad Rio Verde";
+//Comentamos la línea donde tratamos de cambiar el valor de una constante
+//mensaje = "Bienvenido a Ciudad Rio Claro"; 
+//Impresión del valor de la constante "Mensaje"
+console.log( mensaje );
+
+//Caracteristicas de una persona
+const nombre = "Angel";
+const apellidoPaterno = "Ocariz";
+const apellidoMaterno = "Martinez";
+var edad = 16;
+const lugarNacimiento = "Mexico";
+
+//Ejercicio Caracteristicas de un coche 
+
+const marca = "Ford";
+var numero_de_llantas = 4;
+var color = "Rojo";
+var motor = "W 16";
+const modelo = "GT";
+
+console.log( color );
+
+
+
+
+
+// tipo de datos ajua 
+
+var numero = 4500;
+
+var stringTexto = "quesito";
+
+var booleanoV = True; 
+
+var booleanoF = False; 
+
+var condicion = null; 
+
+var valorIndefinido;
+
+const arregloNumeros = [ 1, 2, 3, 4 ];
+const arregloNombres = [ "quesito", "con", "jamon" ];
+const arregloMixto = [ 1, "quesito", 2, "con", 3, "jamon" ];
+
+var objPersona = {
+    nombre: "Angel",
+    apellidoPaterno: "Ocariz",
+    apellidoMaterno: "Martinez",
+    edad = 16,
+    lugarNacimiento = "Mexico"  
+
+};
